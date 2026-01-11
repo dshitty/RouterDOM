4d5ab7d4332c6777e68acbc7167871eeb2281939
SabinGhimireNP <sabinghimire223@gmail.com>
2026-01-11 21:45:21 +0545

    Page not found

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import React from 'react'
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import {Home, Aboutus, Contact} from "./Components/index"
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<h1>Home Page</h1>} />
-        <Route path="/about" element={<h1>About Page</h1>} />
-        <Route path="/contact" element={<h1>Contact Page</h1>} />
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<Aboutus />} />
+        <Route path="/contact" element={< Contact />} />
+        <Route path="*" element={<>Page not Found</>} />
       </Routes>
     </Router>
   )
