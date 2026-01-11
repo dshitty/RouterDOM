652144487dd7e9f1f162e5f3744dfd0443e5a2f5
SabinGhimireNP <sabinghimire223@gmail.com>
2026-01-11 21:31:40 +0545

    Initilization

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import {BrowserRouter as <Router></Router>, Routes, Route} from 'react-router-dom'
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<h1>Home Page</h1>} />
+        <Route path="/about" element={<h1>About Page</h1>} />
+        <Route path="/contact" element={<h1>Contact Page</h1>} />
+      </Routes>
+    </Router>
+  )
+}
+
+export default App
